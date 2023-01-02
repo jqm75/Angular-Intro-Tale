364ec57a76c75d4da1443e7a33315552ff33aee2
Quim <joaquimpujol@gmail.com>
2023-01-02 22:19:48 +0100

    E3 CSS styles

--- src/app/home/escena/escena.component.ts
+++ src/app/home/escena/escena.component.ts
@@ -1,19 +1,49 @@
 import { Component, Input } from '@angular/core';
 import { Sentence } from '../interfaces/sentence';
-
 @Component({
-  selector: 'app-escena',
+
+  selector   : 'app-escena',
   templateUrl: './escena.component.html',
-  styleUrls: ['./escena.component.scss']
+  styleUrls  : ['./escena.component.scss']
+
 })
+
 export class EscenaComponent {
 
-  @Input() arrSentence: Sentence[] = []
+  @Input() arrSentence : Sentence[] = []
   currentSentence : number = 0
 
-  prev(){}
-  next(){}
-  
+  /* prev(){
+
+    if (this.currentSentence === 0) {
+      this.currentSentence = this.arrSentence.length - 1;
+    } else {
+      this.currentSentence--
+    }
+
+  }
+
+  next(){
+
+    if (this.currentSentence === this.arrSentence.length - 1) {
+      this.currentSentence = 0;
+    } else {
+      this.currentSentence++
+    }
+
+  } */    
+
+  navigate = (direction: string) => {
+    if (direction === 'next') {
+      this.currentSentence++;
+    } else if (direction === 'prev') {
+      this.currentSentence--;
+    }
+
+
+
+  }
+
   
 }
 
